c186ad85347fd9b18c9a62b85ed88c192311a6d8
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-01-09 23:33:29 -0800

    practice with unique seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,16 @@ require 'random_data'
 end
 posts = Post.all
 
+Post.find_or_create_by!(title: "This post is unique!", body: "this body is unique too!")
+
 100.times do
   Comment.create!(
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
+
+Comment.find_or_create_by!(post: posts.first, body: "This is a unique comment body!")
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
