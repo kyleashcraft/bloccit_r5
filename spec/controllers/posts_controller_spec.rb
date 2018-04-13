d14ca3057f03780ca0ee138e92f845cf36a0bda4
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-04-12 19:44:29 -0700

    gemfile updated, posts link added to nav, new post button added to posts index, more tests for posts controller spec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -141,4 +141,26 @@ let(:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData
       expect(response).to redirect_to Post.last
     end
   end
+
+  describe "GET edit" do
+    it "returns http success" do
+      get :edit, params: { id: my_post.id }
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, params: { id: my_post.id }
+      expect(response).to render_template :edit
+    end
+
+    it "assigns post to be updated to @post" do
+      get :edit, params: { id: my_post.id }
+
+      post_instance = assigns(:post)
+
+      expect(post_instance.id).to eq my_post.id
+      expect(post_instance.title).to eq my_post.title
+      expect(post_instance.body).to eq my_post.body
+    end
+  end 
 end
