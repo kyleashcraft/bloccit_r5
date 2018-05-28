940f1ecff09f01f4fedee44f9812bc1b03b6df77
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-05-28 12:38:37 -0700

    sign up confirmation, jquery fix

--- Gemfile
+++ Gemfile
@@ -25,7 +25,6 @@ end
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
-gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'thor', '0.19.1'
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,21 @@
 module UsersHelper
-  def is_are(count)
+  def is_are(count) #returns if or are based on plurality of passed object
     if count.abs > 1
       return "are"
     else
       return "is"
     end
-  end 
+  end
+
+  def partial_password(str) #returns partially hidden password for confirmation page
+    newstr = str.dup #duplicate, not pointing to same str value
+    if newstr
+      for i in 0...str.length
+        newstr[i] = "*" if (i != 0 && i != newstr.length - 1)
+      end
+      return newstr
+    else
+      return 'NO PASSWORD'
+    end
+  end
 end
